ca77e675d82e335f46f11642d489f1841bfdd566
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-27 06:06:37 +0300

    container exists but does not have the object

--- src/main/java/com/willis/DemoApp/DemoAppApplication.java
+++ src/main/java/com/willis/DemoApp/DemoAppApplication.java
@@ -2,13 +2,15 @@ package com.willis.DemoApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoAppApplication.class, args);
-		Dev object = new Dev();
+
+		ApplicationContext context=SpringApplication.run(DemoAppApplication.class, args);
+		Dev object = context.getBean(Dev.class);
 		object.build();
 	}
 
