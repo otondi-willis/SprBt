fef4448b2042362e0b5a90a9defbab7c420d3029
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-27 08:27:03 +0300

    use of @Component to for Spring container to create the object

--- src/main/java/com/willis/DemoApp/Laptop.java
+++ src/main/java/com/willis/DemoApp/Laptop.java
@@ -1,5 +1,8 @@
 package com.willis.DemoApp;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Laptop {
     public void compile(){
         System.out.println("Compiling with 404 bugs");
