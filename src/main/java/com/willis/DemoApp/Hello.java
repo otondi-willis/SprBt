11fa64b99979ff51339da7cbb68f426a050a0928
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-27 05:31:27 +0300

    sample rest project that returns string

--- src/main/java/com/willis/DemoApp/Hello.java
+++ src/main/java/com/willis/DemoApp/Hello.java
@@ -1,6 +1,11 @@
 package com.willis.DemoApp;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class Hello {
+
+    @RequestMapping("/")
     public String greet(){
         return "Hello World";
     }
