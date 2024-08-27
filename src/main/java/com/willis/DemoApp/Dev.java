6d0f6fff27973b0dd81d468861c6c9f0c1b3c8d3
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-27 06:07:55 +0300

    use of @Component tells the container to create the object

--- src/main/java/com/willis/DemoApp/Dev.java
+++ src/main/java/com/willis/DemoApp/Dev.java
@@ -1,5 +1,8 @@
 package com.willis.DemoApp;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Dev {
     public void build(){
         System.out.println("working on awesome project");
