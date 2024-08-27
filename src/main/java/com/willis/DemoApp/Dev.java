be04815f69439b3c418f7005bc1a2fedca8168be
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-27 08:27:47 +0300

    using @Autowire to connect Dev class and Laptop class

--- src/main/java/com/willis/DemoApp/Dev.java
+++ src/main/java/com/willis/DemoApp/Dev.java
@@ -1,9 +1,11 @@
 package com.willis.DemoApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
+    @Autowired
     Laptop laptop;
 
 
