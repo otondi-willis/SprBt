90846867fd98f6165b95a042bed936eb2100fc58
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-27 08:21:53 +0300

    typical class creation and instance method calling

--- src/main/java/com/willis/DemoApp/Dev.java
+++ src/main/java/com/willis/DemoApp/Dev.java
@@ -4,7 +4,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
+    Laptop laptop;
+
+
     public void build(){
         System.out.println("working on awesome project");
+        laptop.compile();
     }
 }
---
+++ src/main/java/com/willis/DemoApp/Laptop.java
@@ -0,0 +1,7 @@
+package com.willis.DemoApp;
+
+public class Laptop {
+    public void compile(){
+        System.out.println("Compiling with 404 bugs");
+    }
+}
