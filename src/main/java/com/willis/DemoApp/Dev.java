cb2fb7dd6c119c7d63c19afbd828ddd67df20594
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-27 19:33:11 +0300

    @Primary used for preference in case of confusion

--- src/main/java/com/willis/DemoApp/Dev.java
+++ src/main/java/com/willis/DemoApp/Dev.java
@@ -1,11 +1,13 @@
 package com.willis.DemoApp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
    @Autowired //field injection
+   @Qualifier("laptop")
     private Computer comp;
     //Constructor injection
 //    public Dev(Laptop laptop){
